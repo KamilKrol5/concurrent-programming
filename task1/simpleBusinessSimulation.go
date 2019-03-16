bcc682adb7932370a01680893ad44e37227a98b0
Kamil Król <kamil55525@gmail.com>
2019-03-16 19:43:47 +0100

    Fix mutexes, add talkative and calm mode in one file

--- task1/simpleBusinessSimulation.go
+++ task1/simpleBusinessSimulation.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"math/rand"
+	"os"
+	"sync"
 	"time"
 	// "constants"
 )
 
 const (
 	MAX_ARGUMENT_VALUE   = 500.0
-	MAX_EMPLOYEES        = 40
+	MAX_EMPLOYEES        = 2
 	MAX_CHAIRMEN         = 1
-	MAX_CLIENTS          = 5
+	MAX_CLIENTS          = 1
 	MAX_TASKLIST_SIZE    = 40
 	MAX_STORAGE_CAPACITY = 40
 	EMPLOYEE_SLEEP       = 1000 * time.Millisecond
-	CHAIRMAN_SLEEP       = 700 * time.Millisecond
+	CHAIRMAN_SLEEP       = 400 * time.Millisecond
 	CLIENT_SLEEP         = 2000 * time.Millisecond
+	CALM                 = iota
+	TALKATIVE
 )
 
 var operators = [3]string{"+", "-", "*"}
+var mode = CALM
 
 type task struct {
 	first    float64
@@ -30,6 +36,12 @@ type task struct {
 
 type product struct {
 	value float64
+}
+
+func inform(message ...interface{}) {
+	if mode == TALKATIVE {
+		fmt.Println(message...)
+	}
 }
 
 func doTask(tsk task) (float64, error) {
@@ -51,9 +63,9 @@ func chairman(tasks chan<- task) {
 			first:    rand.Float64() * MAX_ARGUMENT_VALUE,
 			second:   rand.Float64() * MAX_ARGUMENT_VALUE,
 			operator: operators[rand.Intn(len(operators))]}
-		fmt.Println("CHAIRMAN: I've made up a new task! Trying to add it to task list.",newTask)
-		tasks<-newTask
-		fmt.Println("CHAIRMAN: I've added a new task to the task list.")
+		inform("CHAIRMAN: I've made up a new task! Trying to add it to task list.", newTask)
+		tasks <- newTask
+		inform("CHAIRMAN: I've added a new task to the task list.")
 		time.Sleep(CHAIRMAN_SLEEP)
 	}
 }
@@ -63,86 +75,109 @@ func employee(tasks <-chan task, storageChannel chan<- product) {
 		tsk := <-tasks
 		res, err := doTask(tsk)
 		if err != nil {
-			fmt.Println(err.Error()+"EMPLOYEE: Something went wrong in employee routine.")
+			inform(err.Error() + "EMPLOYEE: Something went wrong in employee routine.")
 		} else {
-			storageChannel<-product{value: res}
-			fmt.Println("EMPLOYEE: I've done my task! Result is:",res)
+			storageChannel <- product{value: res}
+			inform("EMPLOYEE: I've done my task! Result is:", res)
 		}
 		time.Sleep(EMPLOYEE_SLEEP)
 	}
 }
 
 func getChannelIfCondition(predicate bool, channel <-chan product) <-chan product {
-	if (predicate) {
+	if predicate {
 		return channel
 	}
 	return nil
 }
 
-func storageManager(storageChannel <-chan product, storage *[]product, display chan<- product) {
+func storageManager(storageChannel <-chan product, storage *[]product, display chan<- product, mutex *sync.Mutex) {
 	for {
+		mutex.Lock()
 		if len(*storage) > 0 {
 			index := rand.Intn(len(*storage)) // [0,len)
 			select {
 			case display <- (*storage)[index]:
-				fmt.Println("STORAGE MANAGER: Product sent to display.")
+				inform("STORAGE MANAGER: Product sent to display.")
 				*storage = append((*storage)[:index], (*storage)[index+1:]...) //remove from storage
-			case newProduct := <-getChannelIfCondition(len(*storage) < MAX_STORAGE_CAPACITY,storageChannel):
-				fmt.Println("STORAGE MANAGER: Adding new product to the storage.")
-				*storage = append(*storage,newProduct)
+			case newProduct := <-getChannelIfCondition(len(*storage) < MAX_STORAGE_CAPACITY, storageChannel):
+				inform("STORAGE MANAGER: Adding new product to the storage.")
+				*storage = append(*storage, newProduct)
 			}
 		} else {
 			select {
 			case newProduct := <-storageChannel:
-				fmt.Println("STORAGE MANAGER: Adding new product to the storage.")
-				*storage = append(*storage,newProduct)
+				inform("STORAGE MANAGER: Adding new product to the storage.")
+				*storage = append(*storage, newProduct)
 			}
 		}
+		mutex.Unlock()
 	}
 }
 
-func maybeOutTaskChannel(predicate bool, channel chan<-task) chan<-task {
-	if (predicate) {
+func maybeOutTaskChannel(predicate bool, channel chan<- task) chan<- task {
+	if predicate {
 		return channel
 	}
 	return nil
 }
 
 func maybeInTaskChannel(predicate bool, channel <-chan task) <-chan task {
-	if (predicate) {
+	if predicate {
 		return channel
 	}
 	return nil
 }
 
-func tasksManager(tasks <-chan task, taskList *[]task, taskOutChannel chan<- task) {
+func tasksManager(tasks <-chan task, taskList *[]task, taskOutChannel chan<- task, mutex *sync.Mutex) {
 	for {
+		mutex.Lock()
 		if len(*taskList) > 0 {
 			select {
 			case newTask := <-maybeInTaskChannel(len(*taskList) < MAX_TASKLIST_SIZE, tasks): //tasks
-				*taskList = append(*taskList,newTask)
-				fmt.Println("TASKS MANAGER: Adding new task to the tasklist.")
+				*taskList = append(*taskList, newTask)
+				inform("TASKS MANAGER: Adding new task to the tasklist.")
 			case taskOutChannel <- (*taskList)[0]:
-				fmt.Println("TASKS MANAGER: Sending new task.")
+				inform("TASKS MANAGER: Sending new task.")
 				*taskList = append((*taskList)[:0], (*taskList)[1:]...)
 			}
 		} else {
 			select {
 			case newTask := <-tasks:
-				*taskList = append(*taskList,newTask)
-				fmt.Println("TASKS MANAGER: Adding new task to the tasklist.")
+				*taskList = append(*taskList, newTask)
+				inform("TASKS MANAGER: Adding new task to the tasklist.")
 			}
 		}
+		mutex.Unlock()
 	}
 }
 
 func client(displayOfProducts <-chan product) {
-	fmt.Println("CLIENT: I am waiting for my product.")
-	myProduct := <-displayOfProducts
-	fmt.Println("CLIENT: Product taken from display, product value: ",myProduct.value)
+	for {
+		inform("CLIENT: I am waiting for my product.")
+		myProduct := <-displayOfProducts
+		inform("CLIENT: Product taken from display, product value: ", myProduct.value)
+		time.Sleep(CLIENT_SLEEP)
+	}
 }
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Wrong number of arguments")
+		return
+	}
+	arg := os.Args[1]
+	switch arg {
+	case "-t", "talkative":
+		mode = TALKATIVE
+	case "-c", "calm":
+		mode = CALM
+	default:
+		fmt.Println("ERROR:", arg, " is not valid argument.")
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	var mutexStorage = &sync.Mutex{}
+	var mutexTasks = &sync.Mutex{}
 	rand.Seed(time.Now().UTC().UnixNano())
 	fmt.Println(MAX_EMPLOYEES)
 	tasks := make([]task, 0, MAX_TASKLIST_SIZE)
@@ -152,19 +187,34 @@ func main() {
 	productChannel := make(chan product)
 	display := make(chan product)
 
-	go tasksManager(tasksChannelIn,&tasks,tasksChannelOut)
-	go storageManager(productChannel,&storage,display)
+	go tasksManager(tasksChannelIn, &tasks, tasksChannelOut, mutexTasks)
+	go storageManager(productChannel, &storage, display, mutexStorage)
 
 	for i := 0; i < MAX_CHAIRMEN; i++ {
 		go chairman(tasksChannelIn)
 	}
 
 	for i := 0; i < MAX_EMPLOYEES; i++ {
-		go employee(tasksChannelOut,productChannel)
+		go employee(tasksChannelOut, productChannel)
 	}
 
 	for i := 0; i < MAX_CLIENTS; i++ {
 		go client(display)
 	}
-	fmt.Scanln()
+	fmt.Println("Options:\n    s - show Storage\n    t - show Task list\n    ")
+	for scanner.Scan() && mode == CALM {
+		var line = scanner.Text()
+		switch line {
+		case "s":
+			mutexStorage.Lock()
+			fmt.Println(storage)
+			mutexStorage.Unlock()
+		case "t":
+			mutexTasks.Lock()
+			fmt.Println(tasks)
+			mutexTasks.Unlock()
+		default:
+			fmt.Println("Wrong option.\nOptions:\n    s - show Storage\n    t - show Task list\n    ")
+		}
+	}
 }
